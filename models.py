47e0253079d443c384a7bacef5da2cbc4f0df362
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-04 22:52:15 +0600

    refactor: uncomment model definitions in Users class

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
-# from database import Base
-# from sqlalchemy import Column, String, Integer, Boolean
+from database import Base
+from sqlalchemy import Column, String, Integer, Boolean
 
-# class Users(Base):
-#     __tablename__ = 'users'
+class Users(Base):
+    __tablename__ = 'users'
 
-#     id = Column(Integer, primary_key=True, index=True)
-#     email = Column(String, unique=True)
-#     username = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    hash_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)  #librarian or member
